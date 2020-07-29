2fac4bd0e4addc38a7a325b92c60ad8b2c7e8703
Aras Sulaiman <arasko87@gmail.com>
2020-07-29 16:31:51 +0100

    Ex No. 2 Done

---
+++ src/Heading.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Heading = () => {
+  return (
+    <div>
+      <header className="App-header">CYF Hotel</header>
+      <img
+        className="image"
+        src="https://image.flaticon.com/icons/svg/139/139899.svg"
+        alt="Hotel Logo"
+      />
+    </div>
+  );
+};
+export default Heading;
