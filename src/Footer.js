1479e6feaffc245e70dafa1ad51a3d02c7aad518
Aras Sulaiman <arasko87@gmail.com>
2020-07-30 10:52:41 +0100

    Ex 4 Done

---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const address = [
+  "123 Fake Street, London, E1 4UD",
+  "hello@fakehotel.com",
+  "0123 456789"
+];
+
+const Footer = () => (
+  <ul>
+    {address.map((props, index) => {
+      return <li key={index}>{props}</li>;
+    })}
+  </ul>
+);
+
+export default Footer;
