bfc0294ad4ea04d1c60e6b3c4744aa0c30db45bc
Aras Sulaiman <arasko87@gmail.com>
2020-08-05 13:35:30 +0100

    Ex 11 Done

--- src/RestaurantButton.js
+++ src/RestaurantButton.js
@@ -3,10 +3,12 @@ import React, { useState } from "react";
 function RestaurantButton() {
   const [orders, setOrders] = useState(0);
 
-  //const OrderOne = () => {setOrders(orders + 1)}
-  function OrderOne() {
+  const OrderOne = () => {
     setOrders(orders + 1);
-  }
+  };
+  //   function OrderOne() {
+  //     setOrders(orders + 1);
+  //   }
   return (
     <div>
       Pizzas: {orders}
