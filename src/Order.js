3d8ca5dad4adcda6bff7d19aa4b7a1481101b609
Aras Sulaiman <arasko87@gmail.com>
2020-08-06 19:01:18 +0100

    EX No 14 Done

--- src/Order.js
+++ src/Order.js
@@ -10,7 +10,7 @@ function Order(props) {
 
   return (
     <li>
-      {props.orderType}: {orders}
+      {props.orderType} : {orders}
       <RestaurantButton addOne={orderOnes} />
     </li>
   );
