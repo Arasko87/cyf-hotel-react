bdf10c71a8e9fe233bd3fc8d82dd1a6c39880afd
Aras Sulaiman <arasko87@gmail.com>
2020-08-13 21:09:32 +0100

    EX 18 and 19 Done

--- src/Bookings.js
+++ src/Bookings.js
@@ -2,12 +2,20 @@ import React, { useState, useEffect } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
 
-const search = searchVal => {
-  console.info("TO DO!", searchVal);
-};
-
 const Bookings = () => {
   const [bookings, setBookings] = useState([]);
+
+  const search = searchVal => {
+    console.info("TO DO!", searchVal);
+
+    const filteredBooking = bookings.filter(name => {
+      return (
+        name.firstName.toUpperCase().includes(searchVal.toUpperCase()) ||
+        name.surname.toUpperCase().includes(searchVal.toUpperCase())
+      );
+    });
+    setBookings(filteredBooking);
+  };
 
   useEffect(() => {
     fetch("https://cyf-react.glitch.me")
--- src/Search.js
+++ src/Search.js
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
 import SearchButton from "./SearchButton";
-const Search = () => {
+const Search = props => {
   const [searchInput, setSearchInput] = useState("");
+
   function handleSearchInput(event) {
     setSearchInput(event.target.value);
-    console.log(event.target.value);
   }
+  function submitHandler(event) {
+    event.preventDefault();
+    props.search(searchInput);
+  }
+
   return (
     <div className="search">
       <div className="page-header">
@@ -13,16 +18,16 @@ const Search = () => {
       </div>
       <div className="row search-wrapper">
         <div className="col">
-          <form className="form-group search-box">
+          <form onSubmit={submitHandler} className="form-group search-box">
             <label htmlFor="customerName">Customer name</label>
             <div className="search-row">
               <input
-                onChange={handleSearchInput}
-                value={searchInput}
                 type="text"
                 id="customerName"
                 className="form-control"
                 placeholder="Customer name"
+                value={searchInput}
+                onChange={handleSearchInput}
               />
               <SearchButton />
             </div>
